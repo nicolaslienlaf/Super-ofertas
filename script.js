c3f12e8b0d1aaccdaf84d1dcbc19198d29ae6a31
nicolaslienlaf <nlienlaf@pregrado.ubo.cl>
2026-06-01 23:09:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const tabla = document.getElementById("cuerpo-tabla");
+    const catalogo = document.getElementById("catalogo-tienda");
     
     fetch("mercado.json")
         .then(response => response.json())
         .then(data => {
             data.productos.forEach(p => {
-                const fila = document.createElement("tr");
-                fila.innerHTML = `
-                    <td><code>#${p.id_alimento}</code></td>
-                    <td><strong>${p.nombre}</strong></td>
-                    <td>${p.marca}</td>
-                    <td style="color: #22c55e; font-weight: bold;">$${p.precio_unidad.toLocaleString('es-CL')}</td>
+                const card = document.createElement("div");
+                card.className = "producto-card";
+                
+                card.innerHTML = `
+                    <span class="id-badge">ID #${p.id_alimento}</span>
+                    <h4>${p.nombre}</h4>
+                    <p class="marca">${p.marca}</p>
+                    <p class="precio">$${p.precio_unidad.toLocaleString('es-CL')}</p>
+                    <button class="btn-simular">Sincronizar Precio</button>
                 `;
-                tabla.appendChild(fila);
+                catalogo.appendChild(card);
             });
         })
-        .catch(err => console.error("Error cargando la API del mercado:", err));
+        .catch(err => console.error("Error cargando el catálogo:", err));
 });
